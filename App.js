54c368d3278d3a990fb910a945a2366c55568fd0
Bibiskop <76591489+Bibiskop@users.noreply.github.com>
2023-04-13 11:42:29 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
 import navBtnImg from './img/img--header/free-icon-globe-558593.png.svg';
 import navBtnImg2 from './img/img--header/free-icon-more-information-2740648.png.svg';;
 
 
 function App() {
+
+    const [value, onChange] = useState(new Date());
+
   return (
     <header class="header backWave">
         <div class="header--logoNav">
@@ -20,10 +25,22 @@ function App() {
         </div>
         <div class="header--filter">
             <div class="filter--input">
-                <div class="filter--blockInput"><input class="filterInput" type="text" placeholder="Откуда"/></div>
-                <div class="filter--blockInput"><input class="filterInput" type="text" placeholder="Куда"/></div>
-                <div class="filter--blockInput"><input class="filterInput" type="text" placeholder="Когда"/></div>
-                <div class="filter--blockInput"><input class="filterInput" type="text" placeholder="Обратно"/></div>
+                <div class="filter--blockInput"><select class="filterInput" type="text" placeholder="Откуда">
+                                <option>Москва</option>
+                                <option>Челябинск</option>
+                                <option>Краснодар</option>
+                                <option>Санкт-Питербург</option>
+                                <option>Новосибирск</option>
+                    </select></div>
+                <div class="filter--blockInput"><select class="filterInput" type="text" placeholder="Куда">
+                                <option>Челябинск</option>
+                                <option>Москва</option>
+                                <option>Краснодар</option>
+                                <option>Санкт-Питербург</option>
+                                <option>Новосибирск</option>
+                    </select></div>
+                <div class="filter--blockInput"><Calendar onChange={onChange} value={value} class="filterInput" type="text" placeholder="Когда"/></div>
+                <div class="filter--blockInput"><Calendar onChange={onChange} value={value} class="filterInput" type="text" placeholder="Обратно"/></div>
                 <div class="filter--blockInput"><input class="filterInput" type="text" placeholder="Пассажиры и класс"/></div>
                 <div class="filter--blockInput filter--button"><button class="filterBtn">Найти билеты</button></div>
             </div>
